908f7705f277eba4fd138d6f24d379b942cec0a9
TinyPin146 <tinypin14@gmail.com>
2021-11-20 22:16:30 +0100

    workingtree cleanup

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,5 +1,22 @@
 const p = document.querySelector('p');
-const imgs = document.querySelectorAll('.items img');
+const imgs = document.querySelectorAll('.item img');
+const item2 = document.querySelector('.item2');
+const item2Image = document.querySelector('img');
+const heading = document.querySelector('h2');
 
-console.log(p);
-console.log(imgs);
+console.log(heading.innerText);
+console.log(heading.textContent);
+
+// heading.textContent = 'Wes is cool';
+// console.dir(heading.textContent);
+// console.log(heading.innerText);
+
+console.log(heading.innerHTML);
+console.log(heading.outerHTML);
+
+const pizzaList = document.querySelector('.pizza');
+console.log(pizzaList.textContent);
+// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+
+pizzaList.insertAdjacentText('afterbegin', '🍕');
+pizzaList.insertAdjacentText('beforeend', '🍕');
