8416f8664a617a4a0afa4778e7760cfafeab07d0
TinyPin146 <tinypin14@gmail.com>
2021-11-20 22:33:27 +0100

    workingtree cleanup

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,22 +1,36 @@
-const p = document.querySelector('p');
-const imgs = document.querySelectorAll('.item img');
-const item2 = document.querySelector('.item2');
-const item2Image = document.querySelector('img');
-const heading = document.querySelector('h2');
+// const p = document.querySelector('p');
+// const imgs = document.querySelectorAll('.item img');
+// const item2 = document.querySelector('.item2');
+// const item2Image = document.querySelector('img');
+// const heading = document.querySelector('h2');
 
-console.log(heading.innerText);
-console.log(heading.textContent);
-
-// heading.textContent = 'Wes is cool';
-// console.dir(heading.textContent);
 // console.log(heading.innerText);
+// console.log(heading.textContent);
 
-console.log(heading.innerHTML);
-console.log(heading.outerHTML);
+// // heading.textContent = 'Wes is cool';
+// // console.dir(heading.textContent);
+// // console.log(heading.innerText);
 
-const pizzaList = document.querySelector('.pizza');
-console.log(pizzaList.textContent);
-// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+// console.log(heading.innerHTML);
+// console.log(heading.outerHTML);
 
-pizzaList.insertAdjacentText('afterbegin', '🍕');
-pizzaList.insertAdjacentText('beforeend', '🍕');
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent);
+// // pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+
+// pizzaList.insertAdjacentText('afterbegin', '🍕');
+// pizzaList.insertAdjacentText('beforeend', '🍕');
+
+// Classes
+
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.remove('cool');
+
+console.log(pic.classList);
+
+function toggleRound() {
+  pic.classList.toggle('round');
+}
+
+pic.addEventListener('click', toggleRound);
