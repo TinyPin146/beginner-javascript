115c76c4685e41a4903d0b0aecb5016cfba674b9
TinyPin146 <tinypin14@gmail.com>
2021-12-26 19:40:32 +0100

    Working tree cleanup

---
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -0,0 +1,82 @@
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d');
+const shakeButton = document.querySelector('.shake');
+const MOVE_AMOUNT = 20;
+
+const { width, height } = canvas;
+
+// * create random starting points
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = MOVE_AMOUNT;
+
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+ctx.beginPath(); // ! start drawing
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
+ctx.stroke();
+
+// ! draw function
+function draw({ key }) {
+  // * increment hue
+  hue += 5;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+  // * start the path
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+
+  // * move depending on what the user did
+  switch (key) {
+    case 'ArrowUp':
+      y -= MOVE_AMOUNT;
+      break;
+
+    case 'ArrowDown':
+      y += MOVE_AMOUNT;
+      break;
+
+    case 'ArrowRight':
+      x += MOVE_AMOUNT;
+      break;
+
+    case 'ArrowLeft':
+      x -= MOVE_AMOUNT;
+      break;
+
+    default:
+      break;
+  }
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
+
+// ! Handler for keys
+function handleKey(e) {
+  //   e.preventDefault();
+  if (e.key.includes('Arrow')) {
+    e.preventDefault();
+    draw({ key: e.key });
+  }
+}
+
+// ! Shake function
+function clearCanvas() {
+  canvas.classList.add('shake');
+  ctx.clearRect(0, 0, width, height);
+  canvas.addEventListener(
+    'animationend',
+    () => {
+      canvas.classList.remove('shake');
+    },
+    { once: true }
+  );
+}
+
+// ! listen for arrow key
+window.addEventListener('keydown', handleKey);
+shakeButton.addEventListener('click', clearCanvas);
